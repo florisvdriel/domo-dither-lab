015e88f95040188d3f9070af8807baa970414062
florisvdriel <studiovandriel@gmail.com>
2026-01-06 17:44:36 +0100

    chore: remove domo warm tones preset

--- app/constants/presets/index.js
+++ app/constants/presets/index.js
@@ -1,12 +1,10 @@
 import { DOMO_DEFAULT } from './domo-default';
 import { DOMO_RGB } from './domo-rgb';
-import { DOMO_WARM_TONES } from './domo-warm-tones';
 import { DOMO_DUOTONE } from './domo-duotone';
 
 // Add new presets here
 export const PRESETS = {
     'domo_default': DOMO_DEFAULT,
     'domo_rgb': DOMO_RGB,
-    'domo_warm_tones': DOMO_WARM_TONES,
     'domo_duotone': DOMO_DUOTONE,
 };
